7d85f9e639d8886b861de99609b428a9c1ce75ba
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-11-08 23:30:58 -0700

    Add Dynamo DB packages.

--- cascade/vite.config.js
+++ cascade/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [['babel-plugin-react-compiler', {}]],
       },
     }),
   ],
+  define: {
+    'process.env.VITE_AWS_REGION': JSON.stringify(process.env.VITE_AWS_REGION ?? ''),
+    'process.env.VITE_AWS_ACCESS_KEY_ID': JSON.stringify(process.env.VITE_AWS_ACCESS_KEY_ID ?? ''),
+    'process.env.VITE_AWS_SECRET_ACCESS_KEY': JSON.stringify(process.env.VITE_AWS_SECRET_ACCESS_KEY ?? ''),
+  },
+  server: {
+    port: 3000,
+  },
 })
